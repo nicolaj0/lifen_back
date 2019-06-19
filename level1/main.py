b1462130c40fc416978c4870f98340cac10c36e8
DESKTOP-3A4BHLQ\junic <junicolas@gmail.com>
2019-06-20 00:57:22 +0200

    refactoring

--- level1/main.py
+++ level1/main.py
@@ -1,13 +1,8 @@
 import json
-# import the pandas library
 from itertools import groupby
 import jsonpickle
 
-
-class Worker:
-    def __init__(self, id, price):
-        self.id = id
-        self.price = price
+from level1.worker import Worker
 
 with open('data.json') as json_file:
     data = json.load(json_file)
@@ -24,7 +19,6 @@ for k, g in groupby(shifts, key=lambda x: x['user_id']):
     workerShiftPrice = (list(worker)[0]['price_per_shift'])
     total = workerShiftPrice * (len(list(g)))
     workersCost.append(Worker(k, total))
-
 
 with open('output.json', 'w', encoding='utf-8') as outfile:
     json.dump({'workers': jsonpickle.encode(workersCost, unpicklable=False)}, outfile, ensure_ascii=False, indent=2)
