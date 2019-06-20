dac3cd2457a824ea91c273b310e4da24bf0197c2
DESKTOP-3A4BHLQ\junic <junicolas@gmail.com>
2019-06-21 00:03:41 +0200

    initial

--- level3/main.py
+++ level3/main.py
@@ -5,9 +5,7 @@ import jsonpickle
 from level1.worker import Worker
 from dateutil.parser import parse
 
-
 costs = {'interne': 126, 'medic': 270}
-costMultiplierPerDOW = {6: 2, 7: 2}
 
 with open('data.json') as json_file:
     data = json.load(json_file)
@@ -15,21 +13,23 @@ with open('data.json') as json_file:
 shifts = data['shifts']
 workers = data['workers']
 
+shifts = filter(lambda x: x['user_id'] is not None, shifts)
 shifts = sorted(shifts, key=lambda x: x['user_id'])
 
 workersCost = []
 
 for k, g in groupby(shifts, key=lambda x: x['user_id']):
-    worker = filter(lambda d: d['id'] == k, workers)
+    worker = list(filter(lambda d: d['id'] == k, workers))
+    shiftsForWorker = list(g)
+    workerStatus = (worker[0]['status'])
+    shiftCostMultiplier = list(map(lambda x: x['start_date'], shiftsForWorker))
+    weekendShifts = list(filter(lambda d: parse(d).weekday() in (5, 6), shiftCostMultiplier))
+    weekdayShifts = list(filter(lambda d: parse(d).weekday() not in (5, 6), shiftCostMultiplier))
 
-    workerStatus = (list(worker)[0]['status'])
-    date_ = list(worker)[0]['start_date']
-    weekday = parse(date_).weekday()
-    shiftCostMultiplier = costMultiplierPerDOW.get(weekday, 1)
     workerStatusPrice = costs[workerStatus]
-
-    total = workerStatusPrice * (len(list(g))) * shiftCostMultiplier
+    total = workerStatusPrice * (len(weekendShifts)*2 + len(weekdayShifts))
     workersCost.append(Worker(k, total))
 print(workersCost)
+
 with open('output.json', 'w', encoding='utf-8') as outfile:
     json.dump({'workers': jsonpickle.encode(workersCost, unpicklable=False)}, outfile, ensure_ascii=False, indent=2)
